cdbfe6f70ad321f59ff34b7ad44ae0494b99707f
b780620 <brandon.greenwell@8451.com>
2021-02-24 15:25:07 -0500

    update tests and rm unnecessary code

--- slowtests/fastshap-force_plot.R
+++ slowtests/fastshap-force_plot.R
@@ -2,26 +2,19 @@ library(fastshap)
 # library(reticulate)
 
 # Set up a Conda environment for new version of shap
-# reticulate::conda_create("r-reticulate-shap-0.37.0")
+version <- "0.37.0"
+env <- paste0("r-reticulate-shap-", version)
+# reticulate::conda_create(env)
 # reticulate::conda_install(
-#   envname = "r-reticulate-shap-0.37.0",
-#   packages = c("shap==0.37.0"),
+#   envname = env,
+#   packages = paste0("shap==", version),
 #   pip = FALSE
 # )
-# reticulate::use_condaenv("r-reticulate-shap-0.37.0", required = TRUE)
-
-# Set up a Conda environment for old version of shap
-# reticulate::conda_create("r-reticulate-shap-0.35.0")
-# reticulate::conda_install(
-#   envname = "r-reticulate-shap-0.35.0",
-#   packages = c("shap==0.35.0"),
-#   pip = FALSE
-# )
-reticulate::use_condaenv("r-reticulate-shap-0.35.0", required =  TRUE)
+reticulate::use_condaenv(env, required = TRUE)
 
 # Check version
-# shap <- import("shap")
-# py_get_attr(shap, name = "__version__")
+shap <- reticulate::import("shap")
+reticulate::py_get_attr(shap, name = "__version__")
 
 # Generate training data from the Friedman 1 benchmark problem
 trn <- gen_friedman(500, seed = 101)
@@ -38,11 +31,11 @@ preds <- pfun(mars, newdata = trn)
 
 # Generate approximate Shapley values for entire training set
 set.seed(102)  # for reproducibility
-shap_all <- explain(mars, X = X, pred_wrapper = pfun, nsim = 1)
+ex <- explain(mars, X = X, pred_wrapper = pfun, nsim = 1)
 
 # Try calling shap.forceplot()
 force_plot(
-  object = shap_all[1L, ],
+  object = ex[1L, ],
   baseline = mean(preds),
   feature_values = X[1L, ]
 )
